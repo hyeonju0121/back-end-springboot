494347670d2f4b5971db63822093227b3177869b
yu hyun ju <a01085249773@gmail.com>
2024-06-03 11:53:24 +0900

    spring security 설정

--- src/main/java/com/mycompany/webapp/security/JwtAuthenticationFilter.java
+++ src/main/java/com/mycompany/webapp/security/JwtAuthenticationFilter.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,6 +25,10 @@ import lombok.extern.slf4j.Slf4j;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Autowired
 	private JwtProvider jwtProvider;
+	
+	@Autowired
+	private AppUserDetailsService appUserDetailsService;
+	
 	
 	// 클라이언트에서 넘어온 access token이 유효한지 안한지 검사를 해야 함
 	// 유효하다면, 안에 있는 사용자 아이디를 받아내야 함
@@ -38,19 +47,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			accessToken = headerValue.substring(7); // "Bearer "
 			log.info("accessToken={}", accessToken);
 		}
-
-		// AccessToken 유효성 검사 (토큰 유효성 검증)
-		Jws<Claims> jws = jwtProvider.validateToken(accessToken);
-		if (jws != null) {
-			// 유효한 경우
-			log.info("AccessToken이 유효함");
-			String userId = jwtProvider.getUserId(jws);
-			log.info("userId={}", userId);
-		} else {
-			// 유효하지 않은 경우
-			log.info("AccessToken이 유효하지 않음 ");
+		
+		// accessToken 이 null 이 아닐 경우, 유효성 검사 수행
+		if(accessToken != null) {
+			// AccessToken 유효성 검사 (토큰 유효성 검증)
+			Jws<Claims> jws = jwtProvider.validateToken(accessToken);
+			if (jws != null) {
+				// 유효한 경우
+				log.info("AccessToken이 유효함");
+				String userId = jwtProvider.getUserId(jws);
+				log.info("userId={}", userId);
+				
+				// userId 를 통해 DB에서 사용자 정보 가져오기
+				// 사용자 실제 정보 얻기
+				AppUserDetails userDetails = (AppUserDetails) appUserDetailsService.loadUserByUsername(userId);
+				// 토큰이 유효하다고 판단되는 경우를 체크하기때문에 패스워드는 null 처리 (아이디, 패스워드, 권한)
+				// 인증 객체 얻기
+				Authentication authentication = 
+						new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				// 스프링 시큐리티에 인증 객체 설정
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				
+			} else {
+				// 유효하지 않은 경우
+				log.info("AccessToken이 유효하지 않음 ");
+			}
 		}
 
+		
 		// 다음 필터를 실행
 		filterChain.doFilter(request, response);
 
--- src/main/java/com/mycompany/webapp/security/WebSecurityConfig.java
+++ src/main/java/com/mycompany/webapp/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,55 +20,59 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
+@EnableMethodSecurity(securedEnabled = true) // @Secured 어노테이션을 사용할 수 있도록 설정
 public class WebSecurityConfig {
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
-	
-	// 인증 필터 체인을 관리 객체로 등록 
+
+	// 인증 필터 체인을 관리 객체로 등록
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// REST API 에서 로그인 폼을 제공하지 않으므로 폼을 통한 로그인 인증을 하지 않도록 설정
 		// 로그인 폼은 프론트엔트 측에서 제공해야함
 		http.formLogin(config -> config.disable());
-		
-		// HttpSession을 사용하지 않기 위한 설정 추가 
-		http.sessionManagement(config -> config.sessionCreationPolicy(
-				SessionCreationPolicy.STATELESS));
-		
+
+		// HttpSession을 사용하지 않기 위한 설정 추가
+		http.sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// 사이트간 요청 위조 방지 비활성화 (GET 방식 이외의 방식 요청은 _crsf 토큰을 요구하기 때문)
+		http.csrf(config -> config.disable());
+
+		// CORS 설정 (크로스 도메인에서 요청할 경우 허가)
+		// = 다른 도메인에서 받은 인증 정보(AccessToken)로 요청할 경우 허가
+		http.cors(config -> {
+		});
+
 		// JWT로 인증이 되도록 필터를 등록
 		// 아이디와 패스워드가 주어져야만 jwtAuthenticationFilter 동작
 		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		
-		// CORS 설정 (크로스 도메인에서 요청할 경우 허가)
-		// = 다른 도메인에서 받은 인증 정보(AccessToken)로 요청할 경우 허가
-		http.cors(config -> {});
-		
+
 		return http.build();
 	}
-	
+
 	// 인증 관리자를 관리 객체로 등록
 	@Bean
-	public AuthenticationManager authenticationManager(
-			AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
-	
+
 	// 권한 계층을 관리 객체로 등록
 	@Bean
 	public RoleHierarchy roleHireHierarchy() {
 		RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
 		hierarchy.setHierarchy("ROLE_ADMIN > ROLE_MANAGER > ROLE_USER");
-		
+
 		return hierarchy;
 	}
-	
+
 	// 다른 도메인(크로스 도메인) 제한 설정 (모든 도메인을 허용하지는 않음)
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		
+
 		// [요청 사이트 제한]
-		// allowedOrigin -> 처음 인증받은 곳을 의미함 
+		// allowedOrigin -> 처음 인증받은 곳을 의미함
 		// * -> 모든 경로 허용
 		configuration.addAllowedOrigin("*");
 		// [요청 방식 제한]
@@ -76,14 +81,14 @@ public class WebSecurityConfig {
 		// * -> 모든 요청을 허용
 		configuration.addAllowedMethod("*");
 		// [요청 헤더 제한]
-		// * -> 모든 헤더를 허용 
+		// * -> 모든 헤더를 허용
 		configuration.addAllowedHeader("*");
-		
+
 		// 모든 URL에 대해 위 설정 내용 적용
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/", configuration);
-		
+
 		return source;
 	}
-	
+
 }
